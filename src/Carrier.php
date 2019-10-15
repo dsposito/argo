e667c41aa05336102212d6509106b214d2cadfd8
AlexP Bell <alex@equities.com>
2019-10-15 10:07:48 -0400

    Revert "Added Laser Ship"
    This reverts commit cd09b1c467d4668b4c852742c660e654115fb8dd.

--- src/Carrier.php
+++ src/Carrier.php
@@ -7,81 +7,76 @@ namespace Argo;
  */
 class Carrier
 {
-	/**
-	 * DHL carrier code.
-	 */
-	const CODE_DHL = 'dhl';
+    /**
+     * DHL carrier code.
+     */
+    const CODE_DHL = 'dhl';
 
-	/**
-	 * FedEx carrier code.
-	 */
-	const CODE_FEDEX = 'fedex';
+    /**
+     * FedEx carrier code.
+     */
+    const CODE_FEDEX = 'fedex';
 
-	/**
-	 * UPS carrier code.
-	 */
-	const CODE_UPS = 'ups';
+    /**
+     * UPS carrier code.
+     */
+    const CODE_UPS = 'ups';
 
-	/**
-	 * USPS carrier code.
-	 */
-	const CODE_USPS = 'usps';
-	/**
-	 * LaserShip carrier code.
-	 */
-	const CODE_LASER_SHIP = 'laser_ship';
+    /**
+     * USPS carrier code.
+     */
+    const CODE_USPS = 'usps';
 
-	/**
-	 * Carrier code.
-	 *
-	 * @var string
-	 */
-	public $code;
+    /**
+     * Carrier code.
+     *
+     * @var string
+     */
+    public $code;
 
-	/**
-	 * Carrier display name.
-	 *
-	 * @var string.
-	 */
-	public $name;
+    /**
+     * Carrier display name.
+     *
+     * @var string.
+     */
+    public $name;
 
-	/**
-	 * Supported carriers.
-	 *
-	 * @var array
-	 */
-	private static $carriers = [
-		self::CODE_DHL => 'DHL',
-		self::CODE_FEDEX => 'FedEx',
-		self::CODE_UPS => 'UPS',
-		self::CODE_USPS => 'USPS',
-		self::CODE_LASER_SHIP => 'Laser Ship',
-	];
+    /**
+     * Supported carriers.
+     *
+     * @var array
+     */
+    private static $carriers = [
+        self::CODE_DHL => 'DHL',
+        self::CODE_FEDEX => 'FedEx',
+        self::CODE_UPS => 'UPS',
+        self::CODE_USPS => 'USPS',
+    ];
 
-	/**
-	 * Initializes the class.
-	 *
-	 * @param string $code The carrier code.
-	 *
-	 * @return void
-	 */
-	public function __construct(string $code)
-	{
-		if (!array_key_exists($code, self::$carriers)) {
-			return false;
-		}
+    /**
+     * Initializes the class.
+     *
+     * @param string $code The carrier code.
+     *
+     * @return void
+     */
+    public function __construct(string $code)
+    {
+        if (!array_key_exists($code, self::$carriers)) {
+            return false;
+        }
 
-		$this->code = $code;
-		$this->name = self::$carriers[$code];
-	}
+        $this->code = $code;
+        $this->name = self::$carriers[$code];
+    }
 
-	/**
-	 * Gets all supported carriers.
-	 *
-	 * @return array
-	 */
-	public static function getAll(): array
-	{
-		return self::$carriers;
-	}
+    /**
+     * Gets all supported carriers.
+     *
+     * @return array
+     */
+    public static function getAll(): array
+    {
+        return self::$carriers;
+    }
 }
